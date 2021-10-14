236a11f232c44034c05239cd00e491a5c6af5d4f
JENNI-ALMARAZ <91512122+JENNI-ALMARAZ@users.noreply.github.com>
2021-10-14 17:43:52 -0500

    SCRIPT SOBRE MÍNIMAS DIMENSIONES DE UNA ALBERCA
    Se desarrolla un problema en donde se tienen que obtener las mínimas dimensiones para una alberca rectangular

---
+++ Problemaalberca.m
@@ -0,0 +1,27 @@
+% Title		:Funcion minimo material para la alberca 
+% Description	:Script para graficar una funcion de el tamaño de una alberca 
+% Author	:Jennifer Almaraz Suarez
+% Date		:20211014
+% Version	:1
+% Usage		:octave> /path/Problemaalberca
+%		:Requiere aplicacion octave, usar su linea de comandos 
+
+% limpiar variable 
+ clear 
+% Ejemplo mínimo de material para una alberca 
+% Rango de 0..2 en i=0.2
+ h=6:0.2:7;
+% Valor de la funcion 
+ Fh= @(h)((2*h) + (80./h));
+% Funcion a plotear  
+ y= (2*h) + (80./h);
+% Funcion para determinar valor minimo
+ rest= fminbnd(Fh,6,7);
+% Dibujar h,y 
+ plot (h,y);
+% Titulo 
+ title(['Minimo material h= ' num2str(rest)]);
+% Etiqueta para x
+ xlabel(['Min h= ' num2str(rest)]);
+% Etiqueta para y 
+ ylabel(['Min h= ' num2str( Fh(rest) )]); 
