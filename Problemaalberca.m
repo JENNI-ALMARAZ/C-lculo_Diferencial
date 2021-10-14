55684b8350caf74166e12de4b5be63ebe3199c04
Astrti Lariza Garcia Castillo <90290485+LarizaG@users.noreply.github.com>
2021-10-14 18:07:45 -0500

    problema de la alberca
    cambio añadido

--- Problemaalberca.m
+++ Problemaalberca.m
@@ -1,6 +1,6 @@
 % Title		:Funcion minimo material para la alberca 
-% Description	:Script para graficar una funcion de el tamaño de una alberca 
-% Author	:Jennifer Almaraz Suarez
+% Description	:Script para graficar una funcion de el tamaÃ±o de una alberca 
+% Author	:Jennifer Almaraz Suarez, Astrit Lariza Garcia Castillo
 % Date		:20211014
 % Version	:1
 % Usage		:octave> /path/Problemaalberca
@@ -8,7 +8,7 @@
 
 % limpiar variable 
  clear 
-% Ejemplo mínimo de material para una alberca 
+% Ejemplo mÃ­nimo de material para una alberca 
 % Rango de 0..2 en i=0.2
  h=6:0.2:7;
 % Valor de la funcion 
